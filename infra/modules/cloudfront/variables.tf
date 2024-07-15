090a9487030ea57f97399762d6e7b368662ef08c
ipluaws <67364414+ipluaws@users.noreply.github.com>
2024-07-15 17:28:05 +0200

    Create variables.tf

---
+++ infra/modules/cloudfront/variables.tf
@@ -0,0 +1,15 @@
+variable "s3_bucket_domain_name" {
+  description = "The domain name of the S3 bucket"
+  type        = string
+}
+
+variable "s3_bucket_id" {
+  description = "The ID of the S3 bucket"
+  type        = string
+}
+
+variable "alb_dns_name" {
+  description = "The DNS name of the ALB"
+  type        = string
+}
+
