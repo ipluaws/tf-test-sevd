93dd91bf3eb37a27e6e3510c5e1d2d52fc544a87
ipluaws <67364414+ipluaws@users.noreply.github.com>
2024-07-16 14:44:45 +0200

    Update HelloController.java

--- backend/src/main/java/com/example/demo/HelloController.java
+++ backend/src/main/java/com/example/demo/HelloController.java
@@ -15,7 +15,7 @@ public class HelloController {
     @GetMapping("/hello")
     public Map<String, String> hello() {
         Map<String, String> response = new HashMap<>();
-        response.put("message", "Hello Greeting message from the BACKEND!");
+        response.put("message", "Hello Greeting message from the BACKEND works!");
         return response;
     }
 }
